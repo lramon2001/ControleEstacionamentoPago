7f3d758b537ac9eed19f5230ae21cd14d8ea8fd9
SwampTG <adriansoareslps@gmail.com>
2021-05-06 23:39:13 -0300

    #Adrian Update gitignore e implementação classe AcessoPorHora

--- estacionamento/src/com/grupo10/estacionamento/classes/AcessoPorHora.java
+++ estacionamento/src/com/grupo10/estacionamento/classes/AcessoPorHora.java
@@ -1,9 +1,17 @@
 package com.grupo10.estacionamento.classes;
+import java.time.Duration;
 
 public class AcessoPorHora extends Acesso {
-    
-    public AcessoPorHora() {
-		
-	}
 
+    @Override
+    public double calculaValor (Duration duracao) {
+        Acesso acesso = new AcessoPorQuinze();
+        double preco = acesso.calculaValor(duracao);
+
+        Duration umaHora = Duration.ofMinutes(60);
+        long nDescontos = (duracao.toMinutes()) % 15;
+
+        return preco - nDescontos * 1;
+
+    }
 }
